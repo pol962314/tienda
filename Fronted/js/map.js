4498e8874007822e3e701033446e67effdbfa7cf
pol <pol@gmail.com>
2020-05-11 23:54:51 -0500

    Marcador de tiendas

--- Fronted/js/map.js
+++ Fronted/js/map.js
@@ -1,97 +1,110 @@
 /* Modificaciones del Marcador */
 let iconMarker = L.icon({
-    iconUrl: 'icon/cursor-64.png',
-    iconSize: [60, 60],
-    iconAnchor: [30, 60],
-    shadowUrl: "https://vivaelsoftwarelibre.com/wp-content/uploads/2020/05/icono_sombra.png",
-    shadowSize: [40, 70],
-    shadowAnchor: [10, 75],
-    popupAnchor: [0, -60],
+  iconUrl: 'icon/cursor-64.png',
+  iconSize: [60, 60],
+  iconAnchor: [30, 60],
+  shadowUrl: "https://vivaelsoftwarelibre.com/wp-content/uploads/2020/05/icono_sombra.png",
+  shadowSize: [40, 70],
+  shadowAnchor: [10, 75],
+  popupAnchor: [0, -60],
 })
 
 /* Marcador global */
 var markerHome
-var markerStores = [
-  ["Jefita",0,0],
-  ["Bandita",0,0],
-  ["Wallpas",0,0]
+
+/* Datos de tienda*/
+var storeFeat = [
+["Jefita",0,0],
+["Bandita",0,0],
+["Wallpas",0,0]
 ]
-var markers = []
-console.log(markerStores)
+/* Marcador de tienda */
+var storeMarkers = []
+console.log(storeFeat)
+
 
 /* Función de geolocalización */
 navigator.geolocation.getCurrentPosition(
-    (pos) => {
-      const { coords } = pos
-      const { latitude, longitude } = coords
-      console.log(latitude,longitude)
-      console.log(this.markerHome)
-      markerStores[0] = ["Jefita",coords.latitude+20,coords.longitude+20]
-      markerStores[1] = ["Bandita",coords.latitude-20,coords.longitude+20]
-      markerStores[2] = ["Wallpas",coords.latitude+10,coords.longitude-20]
-      console.log(markerStores)
-      setMap(latitude,longitude)
-  
-      /* setTimeout(() => {
-        myMap.panTo(new L.LatLng(latitude, longitude))
-      }, 5000) */
-    },
-    (error) => {
-      console.log(error)
-    },
-    {
-      enableHighAccuracy: true,
-      timeout: 5000,
-      maximumAge: 0
-    })
+  (pos) => {
+    const { coords } = pos
+    const { latitude, longitude } = coords
+    console.log(latitude,longitude)
+    console.log(this.markerHome)
 
-    /* Configuración del mapa */
-    function setMap(latitude,longitude){
-        var myMap = L.map('myMap').setView([latitude,longitude], 15)
+    /* Se debe realizar la busqueda de las tiendas */
+    storeFeat[0] = ["Jefita",coords.latitude+0.01,coords.longitude+0.01]
+    storeFeat[1] = ["Bandita",coords.latitude-0.01,coords.longitude+0.01]
+    storeFeat[2] = ["Wallpas",coords.latitude+0.01,coords.longitude-0.01]
+    console.log(storeFeat)
+    setMap(latitude,longitude)
 
-        L.tileLayer(`https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png`, {
-            maxZoom: 30,
-        }).addTo(myMap);
+    /* setTimeout(() => {
+      myMap.panTo(new L.LatLng(latitude, longitude))
+    }, 5000) */
+  },
+  (error) => {
+    console.log(error)
+  },
+  {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  })
 
-        /* Cargar el primer marcador */
-        markerHome = new L.marker([latitude, longitude], { icon: iconMarker, draggable:true});
+  /* Configuración del mapa */
+  function setMap(latitude,longitude){
+      var myMap = L.map('myMap').setView([latitude,longitude], 13)
+
+      L.tileLayer(`https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png`, {
+          maxZoom: 18,
+      }).addTo(myMap);
+
+      /* Cargar el primer marcador */
+      markerHome = new L.marker([latitude, longitude], { icon: iconMarker, draggable:true});
+      myMap.addLayer(markerHome);
+      markerHome.bindPopup("<b>Home</b>").openPopup();
+      console.log(markerHome)
+
+      for (let index = 0; index < this.storeFeat.length; index++) {
+        const element = this.storeFeat[index];
+        console.log(this.storeFeat[index])
+        console.log(element[0])
+
+        this.storeMarkers[index] = new L.marker([element[1],element[2]]);
+        myMap.addLayer(this.storeMarkers[index]);
+        this.storeMarkers[index].bindPopup(element[0]);
+        console.log(this.storeMarkers)
+      }
+      
+      /* Editar el marcador con un click */
+      myMap.on('click', e => {
+        myMap.removeLayer(markerHome)
+        let latLng = myMap.mouseEventToLatLng(e.originalEvent);
+        console.log(markerHome)
+        markerHome = L.marker([latLng.lat, latLng.lng], { icon: iconMarker, draggable:true});
         myMap.addLayer(markerHome);
         markerHome.bindPopup("<b>Home</b>").openPopup();
-        console.log(markerHome)
-        console.log("fa")
 
-        /* Cargar las tiendas */
-        /* for (var i = 0; i < markerStores.length; i++) {
-          markers[i] = new L.marker([markerStores[i][1],markerStores[i][2]],{draggable:true});
-          myMap.addLayer(markers[i])
-          marker[i].bindPopup(markerStores[i][0])
-          console.log(markers[i])
+        /* Se debe actualizar las tiendas */
+        this.storeFeat[0] = ["Jefita",latLng.lat+0.01, latLng.lng+0.01]
+        this.storeFeat[1] = ["Bandita",latLng.lat+0.01, latLng.lng-0.01]
+        this.storeFeat[2] = ["Wallpas",latLng.lat-0.01, latLng.lng-0.01]
+        console.log(this.storeFeat)
+
+
+        for (let index = 0; index < this.storeFeat.length; index++) {
+          const element = this.storeFeat[index];
+          myMap.removeLayer(this.storeMarkers[index])
+          this.storeMarkers[index] = new L.marker([element[1],element[2]]);
+          myMap.addLayer(this.storeMarkers[index]);
+          this.storeMarkers[index].bindPopup(element[0]);
+          console.log(this.storeMarkers)
         }
-        console.log(markers) */
 
-        /* Editar el marcador con un click */
-        myMap.on('click', e => {
-          myMap.removeLayer(markerHome)
-          let latLng = myMap.mouseEventToLatLng(e.originalEvent);
-          console.log(markerHome)
-          markerHome = L.marker([latLng.lat, latLng.lng], { icon: iconMarker, draggable:true});
-          myMap.addLayer(markerHome);
-          markerHome.bindPopup("<b>Home</b>").openPopup();
+      }
+    )
 
-          /* Editar las tiendas */
-          /* for (var i = 0; i < markerStores.length; i++) {
-            myMap.removeLayer(markers[i])
-            markers[i] = new L.marker([markerStores[i][1],markerStores[i][2]],{draggable:true});
-            myMap.addLayer(markers[i])
-            marker[i].bindPopup(markerStores[i][0])
-          } */
-
-          }
-        )
-
-    }
-
-    
+  }
         
    
 /* 
